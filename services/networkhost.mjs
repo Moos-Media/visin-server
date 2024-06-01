1599e46d160faf44ca1de525789dabaa33aedd1b
Lukas Moos <lukas.stephan.moos@muk.thm.de>
2024-06-01 18:10:36 +0200

    Added Login for Debug Site.

--- services/networkhost.mjs
+++ services/networkhost.mjs
@@ -3,6 +3,8 @@ import express, { request, response } from "express";
 class Networkhost {
   constructor(buildport, builddirectory, debugport, debugdirectory) {
     //Set up Servers with passed in values
+    //
+    //Build
     const buildserver = express();
     buildserver.listen(buildport, () =>
       console.log("Build Server Listening at " + buildport)
@@ -10,12 +12,27 @@ class Networkhost {
     buildserver.use(express.static(builddirectory));
     buildserver.use(express.json({ limit: "1mb" }));
 
+    //Debug
     const debugserver = express();
     debugserver.listen(debugport, () =>
       console.log("Debug Server Listening at " + debugport)
     );
     debugserver.use(express.static(debugdirectory));
     debugserver.use(express.json({ limit: "1mb" }));
+
+    //
+    //RequestHandling
+    //
+    debugserver.post("/api/login", (request, response) => {
+      const data = request.body;
+
+      //Check for access
+      if (data.username == "admin" && data.passkey == "admin") {
+        response.json({ result: true });
+      } else {
+        response.json({ result: false });
+      }
+    });
   }
 }
 
